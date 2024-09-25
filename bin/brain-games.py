88ebe5d61d4c854cdf9c71e6937b120e3d728548
dimitrius <dkuleshov0506@gmail.com>
2024-09-25 23:41:24 +0300

    some changes

--- bin/brain-games.py
+++ bin/brain-games.py
@@ -1,3 +1,4 @@
-from src.cli import welcome
+from src.cli import welcome, lcm_alg
 
 print(welcome())
+print(lcm_alg())
--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,13 @@
+import random
+
+
 def welcome():
     print("Welcome to the Brain Games!\n" "May I have your name?")
     name = input()
     return f"Hello, {name}!"
 
+
+def lcm_alg():
+    print("Find the smallest common multiple of given numbers.")
+    numbers = random.sample(range(0, 500), 3)
+    return numbers
