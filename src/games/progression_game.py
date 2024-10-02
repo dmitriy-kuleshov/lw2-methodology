bb0123b935d7cc488b90d7f5b8764afe2f5e04c9
dimitrius <dkuleshov0506@gmail.com>
2024-10-02 23:13:23 +0300

    добавил файлы, выделил общее для игр

---
+++ src/games/progression_game.py
@@ -0,0 +1,19 @@
+import random
+from src.manage import attempt_quiz
+
+
+def progression_question():
+    start_point = random.randint(1, 9)
+    mystery_point = random.randint(0, 5)
+    sequence = [start_point * (start_point ** i) for i in range(6)]
+    answer = sequence[mystery_point]
+    formatted_question = ' '.join(map(str, sequence)).replace(str(answer), "..", 1)
+    return {'sequence': sequence, 'answer': answer, 'formatted_question': formatted_question}
+
+
+def progression_answer(question_data):
+    return question_data['answer']
+
+
+def progression(username):
+    attempt_quiz(username, progression_question, progression_answer)
